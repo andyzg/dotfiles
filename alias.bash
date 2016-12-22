17071a70ff44c65631405d80e1c9a71a92bfc267
Andy Zhang <andzhng@gmail.com>
2016-12-22 15:10:22 -0500

    Add files via upload

---
+++ alias.bash
@@ -0,0 +1,45 @@
+#!/bin/bash
+alias ls="ls -G"
+
+alias question="vim ~/Documents/Github/school/2B/questions.md"
+alias quest="question"
+alias qst="question"
+
+# Shortcuts to important stuff
+alias nus="cd ~/Dropbox/Work/NUS/"
+alias hackerapi="cd ~/Documents/Github/hackerapi.com/"
+alias team="cd ~/Documents/Github/team.hackthenorth.com/"
+alias 2b="cd ~/Documents/Github/school/2B/"
+alias 2B="2b"
+alias gh="cd ~/Documents/Github/"
+
+# Git shortcuts
+alias gsh="git show "
+alias gst="git status "
+alias gsd="git checkout -- "
+alias gl="git log"
+alias gpush="git push"
+alias gpull="git pull"
+alias grebase="git rebase"
+alias gstash="git stash "
+alias gdrop="git stash drop"
+alias gpop="git stash pop "
+alias gdiff="git diff"
+alias gb="git checkout -B "
+alias gc="git checkout "
+alias gaa="git add -A"
+alias gcm="git commit -m "
+alias gca="git commit --amend"
+alias ggr="git grep "
+
+alias git-alias="cat ~/.bash/alias.bash | grep git | cut -d ' ' -f 2- | sed '$ d'"
+alias editalias="vim ~/.bash/alias.bash"
+
+alias vim="mvim"
+alias vi="vi"
+
+alias work="cd /Users/andyzg/Dropbox/Work/University\ of\ Waterloo/Work\ term\ reports/3B-work-term-report"
+alias 3b="3B"
+alias 3B="cd /Users/andyzg/Dropbox/Work/University\ of\ Waterloo/3B/"
+
+alias templatelatex="cp ~/.templates/latex.tex "
